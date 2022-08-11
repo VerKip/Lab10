77c1504b02fde5ea16f4432b8866dac9a2cbb868
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-11 19:25:26 +0300

    Add project files.

---
+++ Lab10/Angle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get { return min; }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+
+        public int Sec
+        {
+            get { return sec; } 
+            set {
+                Min += value / 60;
+                sec = value % 60; }
+        }
+
+        public Angle (int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }   
+
+        public double ToRadians ()
+        {
+            return (gradus + (min + sec/60)/60)*Math.PI/180;    
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите градусы, минуты, секунды");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            int min_ = Convert.ToInt32(Console.ReadLine());
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus_, min_, sec_);
+            double totalAngle = angle.ToRadians();
+            Console.WriteLine(totalAngle);
+            Console.ReadKey();
+        }
+    }
+}
